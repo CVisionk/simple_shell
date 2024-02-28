4a04211c26783c819f1237be20cabe1d61938064
Collen <damd819@gmail.com>
2024-02-28 10:51:34 +0200

    betty fix

--- shell.c
+++ shell.c
@@ -1,8 +1,14 @@
-include "main.h"
+#include "main.h"
 
 #define MAX_INPUT_LENGTH 1024
 #define MAX_ARGS 64
 
+/**
+ * char **splitter - split input into array of strings.
+ * @input: input to split
+ *
+ * Return: array of strings
+ */
 char **splitter(char *input)
 {
 	char *token;
@@ -13,7 +19,7 @@ char **splitter(char *input)
 	{
 		fprintf(stderr, "Memory allocation failed\n");
 		exit(EXIT_FAILURE);
-		}
+	}
 
 	token = strtok(input, " ");
 	arg_index = 0;
@@ -21,75 +27,96 @@ char **splitter(char *input)
 	{
 		args[arg_index++] = strdup(token);
 		token = strtok(NULL, " ");
-		}
+	}
 	args[arg_index] = NULL;
 	return (args);
 }
 
+
+/**
+ * void free args - free memory
+ * @args: memory to free
+ *
+ * Returns: void
+ */
 void free_args(char **args)
 {
 	for (int i = 0; args[i] != NULL; i++)
 	{
 		free(args[i]);
- }
- free(args);
+	}
+	free(args);
 }
 
-void execute_command(char** args)
+
+/**
+ * void execute_command - execute args
+ * @args: arguments to execute
+ *
+ * Returns: void
+ */
+void execute_command(char **args)
 {
- pid_t pid = fork();
- if (pid < 0)
-	{
-  fprintf(stderr, "Fork failed\n");
-  exit(EXIT_FAILURE);
- } else if (pid == 0)
-	{
- 	/* Child process */
- 	if (execve(args[0], args, NULL) == -1)
-		{
-  	fprintf(stderr, "./shell: No such file or directory\n");
-  	exit(EXIT_FAILURE);
-  }
+	pid_t pid = fork();
 
- } else
+	if (pid < 0)
 	{
- 	 /* Parent process */
-   wait(NULL); /* Wait for child to finish */
- }
+		fprintf(stderr, "Fork failed\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		/* Child process */
+		if (execve(args[0], args, NULL) == -1)
+		{
+			fprintf(stderr, "./shell: No such file or directory\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* Parent process */
+		wait(NULL); /* Wait for child to finish */
+	}
 }
 
 
-int main()
+
+/**
+ * void main - no description
+ *
+ *
+ * Returns: int
+ */
+
+int main(void)
 {
 	size_t len = 0;
- ssize_t read;
+	ssize_t read;
 	char *line = NULL;
-	char ** args;
+	char **args;
 
 	printf("#cisfun$ ");
-	while ((read = getline(&line, &len, stdin))!= -1)
+	while ((read = getline(&line, &len, stdin)) != -1)
 	{
 		if (read == -1 && len == 0)
 		{
-   fprintf(stderr, "Memory allocation failed\n");
-   exit(EXIT_FAILURE);
-  }
-  line[strcspn(line, "\n")] = 0;
-    
-		args = splitter(line);
-	/*for (int i = 0; args[i] != NULL; i++) 
-	{
-        printf("%s\n", args[i]);
-		
-    }*/
-	if(args[1] == NULL)
-	{
-		execute_command(args);
+			fprintf(stderr, "Memory allocation failed\n");
+			exit(EXIT_FAILURE);
+		}
+		line[strcspn(line, "\n")] = 0;
 
-	}else
-		fprintf(stderr, "./shell: No such file or directory\n");
-    printf("#cisfun$ ");
-	free_args(args);
-    }
+		args = splitter(line);
+
+		if (args[1] == NULL)
+		{
+			execute_command(args);
+		}
+		else
+			fprintf(stderr, "./shell: No such file or directory\n");
+
+		printf("#cisfun$ ");
+		free_args(args);
+	}
 	return (0);
 }
